b7f06651fa5f62d21c321bdbe91724353608d1f4
prakashchelluri <prakashchelluri@gmail.com>
2020-06-01 22:10:32 +0530

    created new files

---
+++ sample.py
@@ -0,0 +1,12 @@
+import math
+#print(math.sqrt(16))
+#print(math.tan(90))
+#print(math.pi)
+#h=("Python practice program")
+#print(h)
+#added from mac
+
+
+r=int(input('Enter the radius:'))
+area=math.pi*r**2
+print(area)
